a6296a909e086905569608db349e849fef42a2d4
jxalfoudqt <jxalfoudqt0@gmail.com>
2019-06-21 03:37:45 +0100

    add file

---
+++ 1D_evaporation_without_diffusion_eos4/execfile_file.py
@@ -0,0 +1,7 @@
+import os
+cwd = os.getcwd()
+
+#execfile(cwd+'/python_code/Evaporation_1D_eos4_pre_process.py')
+#execfile(cwd+'/python_code/parsing_inputfile.py')
+#execfile(cwd+'/python_code/running_model.py')
+#execfile(cwd+'/python_code/Evaporation_1D_eos4_post_process.py')
