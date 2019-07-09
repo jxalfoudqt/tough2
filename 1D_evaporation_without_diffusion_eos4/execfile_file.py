052d7f699eea47c6194fdcd982af79058d6094d2
jxalfoudqt <jxalfoudqt0@gmail.com>
2019-07-09 06:36:29 +0100

    new file

--- 1D_evaporation_without_diffusion_eos4/execfile_file.py
+++ 1D_evaporation_without_diffusion_eos4/execfile_file.py
@@ -1,7 +1,8 @@
 import os
 cwd = os.getcwd()
 
-execfile(cwd+'/python_code/Evaporation_1D_eos4_pre_process.py')
-#execfile(cwd+'/python_code/parsing_inputfile.py')
-execfile(cwd+'/python_code/running_model.py')
-execfile(cwd+'/python_code/Evaporation_1D_eos4_post_process.py')
+#execfile(cwd+'/python/pre_process.py')
+#execfile(cwd+'/python/parsing_inputfile.py')
+#execfile(cwd+'/python/running_model.py')
+#execfile(cwd+'/python/parsing_outputfile.py')
+execfile(cwd+'/python/post_process.py')
