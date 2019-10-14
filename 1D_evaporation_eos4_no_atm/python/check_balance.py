c230ec66df197a4b4da949ec728dff8810c7b8e2
jxalfoudqt <jxalfoudqt0@gmail.com>
2019-10-14 01:32:30 -0400

    why needs this

--- 1D_evaporation_eos4_no_atm/python/check_balance.py
+++ 1D_evaporation_eos4_no_atm/python/check_balance.py
@@ -42,43 +42,46 @@ vapor_saturated_pressure_pa                      = 611*np.exp(17.27*(temperature
 vapor_pressure_pa_calculated                     = vapor_saturated_pressure_pa*relative_humidity_percent
 vapor_density_kgPm3_calculated                   = vapor_pressure_pa_calculated*water_molecular_weight/(R_value*temperature_degree)
 vapor_density_gradient_chenming                  = np.gradient(vapor_density_kgPm3_calculated,axis=0)/dz
-diffusion_coefficient_chenming                   = 2.29e-5*((temperature_degree+T_kelven)/T_kelven)**1.8
-diffusion_calculation_chenming                   = porosity**(4./3)*(gas_saturation)**(10./3)*gas_density_kgPm3*diffusion_coefficient_chenming*vapor_density_gradient_chenming*dat.grid.connectionlist[0].area
+#diffusion_coefficient_chenming                   = 2.29e-5*((temperature_degree+T_kelven)/T_kelven)**1.8
+diffusion_calculation_chenming                   = porosity**(4./3)*(gas_saturation)**(10./3)*gas_density_kgPm3*diffusion_coefficient*vapor_density_gradient_chenming*dat.grid.connectionlist[0].area
 diffusion_calculation_relative_error_chenming    = np.abs((diffusion_calculation_chenming[1:]+vapor_diff_flow_kgPs)/vapor_diff_flow_kgPs)
 
 i=9
-fig=plt.figure()
-ax1=plt.subplot(141)
-ax1.plot(diffusion_calculation[1:,i],connection_value,'b-',-vapor_diff_flow_kgPs[:,i],connection_value,'k-')
-plt.xlabel('vapor_diff_flow (kgPs)')
-plt.ylabel('x (m)')
-plt.ylim(1.1,-0.1)
-# plt.xlim(np.nanmin(initial_porosity),np.nanmax(initial_porosity))
-ax1.set_xscale('log')
-
-ax1=plt.subplot(142)
-ax1.plot(-vapor_diff_flow_kgPs[:,i],connection_value,'k-',diffusion_calculation_chenming[1:,i],connection_value,'r-')
-plt.xlabel('vapor_diff_flow (kgPs)')
-#plt.ylabel('x (m)')
-plt.ylim(1.1,-0.1)
-# plt.xlim(np.nanmin(initial_porosity),np.nanmax(initial_porosity))
-ax1.set_xscale('log')
-
-ax1=plt.subplot(143)
-ax1.plot(vapor_pressure_pa_calculated[:,i],element_value,'r-',vapor_pressure_pa[:,i],element_value,'k-')
-plt.xlabel('vapor_pre (Pa)')
-#plt.ylabel('x (m)')
-plt.ylim(1.1,-0.1)
-# plt.xlim(np.nanmin(initial_porosity),np.nanmax(initial_porosity))
-#ax1.set_xscale('log')
-
-ax1=plt.subplot(144)
-ax1.plot(vapor_density_kgPm3_calculated[:,i],element_value,'r-',vapor_density_kgPm3[:,i],element_value,'k-')
-plt.xlabel('vapor_density (kgPm3)')
-#plt.ylabel('x (m)')
-plt.ylim(1.1,-0.1)
-# plt.xlim(np.nanmin(initial_porosity),np.nanmax(initial_porosity))
-#ax1.set_xscale('log')
-plt.rcParams.update({'font.size': 8})
-fig.tight_layout()
-plt.savefig('figure/diffusion_balance_check.png',dpi=300) 
+while i==9:#<lst.num_times:
+    
+    fig=plt.figure()
+    ax1=plt.subplot(141)
+    ax1.plot(diffusion_calculation[1:,i],connection_value,'b-',-vapor_diff_flow_kgPs[:,i],connection_value,'k-')
+    plt.xlabel('vapor_diff_flow (kgPs)')
+    plt.ylabel('x (m)')
+    plt.ylim(1.1,-0.1)
+    # plt.xlim(np.nanmin(initial_porosity),np.nanmax(initial_porosity))
+    ax1.set_xscale('log')
+    
+    ax1=plt.subplot(142)
+    ax1.plot(-vapor_diff_flow_kgPs[:,i],connection_value,'k-',diffusion_calculation_chenming[1:,i],connection_value,'r-')
+    plt.xlabel('vapor_diff_flow (kgPs)')
+    #plt.ylabel('x (m)')
+    plt.ylim(1.1,-0.1)
+    # plt.xlim(np.nanmin(initial_porosity),np.nanmax(initial_porosity))
+    ax1.set_xscale('log')
+    
+    ax1=plt.subplot(143)
+    ax1.plot(vapor_pressure_pa_calculated[:,i],element_value,'r-',vapor_pressure_pa[:,i],element_value,'k-')
+    plt.xlabel('vapor_pre (Pa)')
+    #plt.ylabel('x (m)')
+    plt.ylim(1.1,-0.1)
+    # plt.xlim(np.nanmin(initial_porosity),np.nanmax(initial_porosity))
+    #ax1.set_xscale('log')
+    
+    ax1=plt.subplot(144)
+    ax1.plot(vapor_density_kgPm3_calculated[:,i],element_value,'r-',vapor_density_kgPm3[:,i],element_value,'k-')
+    plt.xlabel('vapor_density (kgPm3)')
+    #plt.ylabel('x (m)')
+    plt.ylim(1.1,-0.1)
+    # plt.xlim(np.nanmin(initial_porosity),np.nanmax(initial_porosity))
+    #ax1.set_xscale('log')
+    plt.rcParams.update({'font.size': 8})
+    fig.tight_layout()
+    plt.savefig('figure/diffusion_balance_check'+str(i+1)+'.png',dpi=300) 
+    i+=1
