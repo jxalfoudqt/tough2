7d2c498d1a8cd9370208cb7084f8a0cb4fe56505
jxalfoudqt <jxalfoudqt0@gmail.com>
2020-06-02 03:00:19 -0400

    dp model no tide

---
+++ dp_model_EOS7_triangle_notide/python/running_model.py
@@ -0,0 +1,14 @@
+from t2listing import *
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+import os
+from t2data import *
+from mpl_toolkits.mplot3d import Axes3D
+
+dat.title = 'dp_model_flow'
+#--- run the model ------------------------------------
+
+#os.system(" tough2 -to sam6_0.listing sam6 4")
+os.system(' tough2 -to '+dat.title+'.listing '+dat.title+' 7')
+#os.system(' tough2 -fi atmos.dat -to '+dat.title+'_0.listing '+dat.title+'_TIMBC 7')
