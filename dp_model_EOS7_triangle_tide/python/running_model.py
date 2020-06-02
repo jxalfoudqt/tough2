2a4716929221b208820ebcedf33c8f7664c6cc71
jxalfoudqt <jxalfoudqt0@gmail.com>
2020-06-02 03:01:47 -0400

    do model with tide

---
+++ dp_model_EOS7_triangle_tide/python/running_model.py
@@ -0,0 +1,14 @@
+from t2listing import *
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+import os
+from t2data import *
+from mpl_toolkits.mplot3d import Axes3D
+
+dat.title = 'dp_model_flow_TIMBC'
+#--- run the model ------------------------------------
+
+#os.system(" tough2 -to sam6_0.listing sam6 4")
+#os.system(' tough2 -to '+dat.title+'.listing '+dat.title+' 7')
+os.system(' tough2 -fi atmos.dat -to '+dat.title+'.listing '+dat.title+' 7')
